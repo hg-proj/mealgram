95fd7e9aa1575cf1f3e9ecf99bf41de802d71696
Hangyeol Park <hangyeol0019@gmail.com>
2026-09-15 17:58:24 +0900

    chore: Spring Boot 프로젝트 초기 생성 (#4)

---
+++ backend/src/main/java/com/mealgram/MealgramApplication.java
@@ -0,0 +1,13 @@
+package com.mealgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealgramApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mealgram/MealgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
